437dfb694c925d0915bc6da8807d4bba1b0b5a90
Nicholas McDonald <nmmcdona@ucsd.edu>
2016-12-15 14:13:18 -0800

    Initial commit.

--- tipify/ViewController.swift
+++ tipify/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+        
+    }
 
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let billTotal = Double(billTextField.text!) ?? 0
+        let tip = billTotal*tipPercentages[tipSegmentedControl.selectedSegmentIndex]
+        let total = billTotal + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
