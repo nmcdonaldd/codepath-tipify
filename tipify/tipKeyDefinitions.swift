2f482d48c6606aa8e5b8fa77fac231688469fb67
Nicholas McDonald <nmmcdona@ucsd.edu>
2016-12-15 16:56:41 -0800

    Added some new files for keyDefinitions and app features. Also added a first-time app launch dialog.

---
+++ tipify/tipKeyDefinitions.swift
@@ -0,0 +1,9 @@
+//
+//  tipKeyDefinitions.swift
+//  tipify
+//
+//  Created by Nick McDonald on 12/15/16.
+//  Copyright © 2016 Nick McDonald. All rights reserved.
+//
+
+let tipFirstTimeAppLaunch = "tipFirstTimeAppLaunch"
--- tipify/tipViewController.swift
+++ tipify/tipViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class tipViewController: UIViewController {
 
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
